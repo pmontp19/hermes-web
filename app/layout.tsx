c4a6130a3fc2afde790a3bc03aaa23eeb740d8e5
pmontp19 <peremontpeo@gmail.com>
2025-02-26 18:09:28 +0100

    refactor: remove unused import for Footer component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { Footer } from '@/components/Footer'
 import { HeaderWrapper } from '@/components/HeaderWrapper'
 import { FooterWrapper } from '@/components/FooterWrapper'
 
