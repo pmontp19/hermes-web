f6aac45ae03ad6a6a9626376ad92ed370bcb08fc
pmontp19 <peremontpeo@gmail.com>
2025-03-14 13:46:37 +0100

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://your-website-url.com', // Replace this with your actual website URL
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
